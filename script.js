9ce82f97f203aacbd95621f746fd675e990eae05
antoniogrecoo <queirozgreco@gmail.com>
2025-02-24 20:10:38 -0400

    alterações sinistrasssss

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function toggleMenu() {
     document.querySelector(".nav-links").classList.toggle("active");
 }
+document.addEventListener("DOMContentLoaded", function() {
+    let botao = document.getElementById("botao-flutuante");
+
+    // Remove o botão após 2 segundos
+    setTimeout(() => {
+        botao.style.animation = "fadeOut 0.5s ease-in-out";
+        setTimeout(() => botao.style.display = "none", 500); // Remove o botão após o fade-out
+    }, 2000);
+});
